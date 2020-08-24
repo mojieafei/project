41f4f4729c119942c376b9775d168052c20c3acc
dave <daveafei@hotmail.com>
2020-08-24 08:43:39 +0800

    inti

--- project-web/src/main/java/com/ymt/controller/IndexController.java
+++ project-web/src/main/java/com/ymt/controller/IndexController.java
@@ -1,15 +1,16 @@
 package com.ymt.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping(path = "/")
 public class IndexController {
 
     @GetMapping
     public String getUser(){
-        return "It is works!";
+        return "scratch";
     }
 }
---
+++ project-web/src/main/java/com/ymt/controller/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ymt.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.context.WebApplicationContext;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
+}
